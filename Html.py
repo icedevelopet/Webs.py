0ecc455ef06ddc02d510aa9b51ccde12732a1d03
icedevelopet <jorgelaramedina17@gmail.com>
2026-04-26 01:11:46 -0500

    Update Html.py

--- Html.py
+++ Html.py
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+ </head>
 <title>JOIK STUDIO WEB</title>
 <body>
 
 <h1>JOIK STUDIOS PROYECTS</h1>
 <p>proyectos actuales y futuros de joik studios</p>
 
+p{color:red;}
